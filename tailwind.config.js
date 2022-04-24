e262855a7f1bc07fef60c60b468f7cebdf6f8c83
Osman Rafi <33856265+Osman-Rafi@users.noreply.github.com>
2022-04-25 00:54:05 +0600

    fix: tailwind config fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
+// tailwind.config.js
 module.exports = {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
+  purge: [
+    './resources/**/*.blade.php',
+    './resources/**/*.js',
+    './resources/**/*.vue',
+  ],
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
